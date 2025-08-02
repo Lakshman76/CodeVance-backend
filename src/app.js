f24a6264c60f97496797f6b9d5fd15de8fcc3ee0
Lakshman76 <lakshmankumar2603@gmail.com>
2025-08-02 14:25:20 +0530

    Basic setup using express

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello from CodeVance</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1> About CodeVance</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
