ef37faafbbc5d990233a3908613f0431c542670d
Lakshman76 <lakshmankumar2603@gmail.com>
2025-08-03 12:23:22 +0530

    add : middleware (isLoggedIn)

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { signup, login, logout } = require("../controllers/auth");
+const isLoggedIn = require("../middleware/auth");
 
 const route = express.Router();
 
 route.post("/signup", signup);
 route.post("/login", login);
-route.post("/logout", logout);
+route.post("/logout", isLoggedIn, logout);
 
 module.exports = route;
